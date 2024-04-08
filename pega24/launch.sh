2dc380a1616ee8fc637255c037203676a284ce75
kompot <dmitry.kolesnikov@k-expert.com>
2024-04-08 18:17:13 +0200

    export PEGA_HOST

--- pega24/launch.sh
+++ pega24/launch.sh
@@ -2,12 +2,12 @@
 # get hostname, tier and start
 cd "$(dirname "$0")"
 PEGA_HOST=$(cat /etc/hostname)
+export PEGA_HOST
 if [[ $PEGA_HOST == *"-cdh" ]]; then
     export PEGA_TIER=cdh
     export PEGA_TYPE=ADM,Batch,RealTime,RTDG,Search,BackgroundProcessing
     export CASSANDRA_CLUSTER=true
     export CASSANDRA_NODES=cassandra
-
     docker compose --profile cdh up -d 
 else
     docker compose up -d
